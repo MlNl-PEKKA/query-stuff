ddf930e0a4bc17f370a3c158cc00564314f2819d
MlNl-PEKKA <punithg360@gmail.com>
2025-01-21 23:31:20 +0530

    chore: remove redudant codeblock

--- src/createProxyNode.ts
+++ src/createProxyNode.ts
@@ -15,41 +15,36 @@ export const createProxyNode = <T extends Node>(
 ): ProxyNode<T> => {
   return new Proxy(node, {
     get: (target, p, receiver) => {
-      if (isNodeFunction(target)) {
-        throw new Error("Only non-functions allowed");
-      }
-      if (!isString(p)) {
-        throw new Error("Only string properties allowed");
-      }
-      if (p === "_key") {
-        return keys;
-      }
-      if (isQueryNode(target)) {
-        return Reflect.get({ ...target, queryKey: keys }, p, receiver);
-      }
-      if (isMutationNode(target)) {
-        return Reflect.get({ ...target, mutationKey: keys }, p, receiver);
-      }
-      const nextTarget = target[p];
-      const nextKeys = [...keys, p];
-      if (isNode(nextTarget)) {
-        return createProxyNode(nextTarget, nextKeys);
+      if (!isNodeFunction(target) && isString(p)) {
+        if (p === "_key") {
+          return keys;
+        }
+        if (isQueryNode(target)) {
+          return Reflect.get({ ...target, queryKey: keys }, p, receiver);
+        }
+        if (isMutationNode(target)) {
+          return Reflect.get({ ...target, mutationKey: keys }, p, receiver);
+        }
+        const nextTarget = target[p];
+        const nextKeys = [...keys, p];
+        if (isNode(nextTarget)) {
+          return createProxyNode(nextTarget, nextKeys);
+        }
       }
     },
     apply: (target, _thisArg, argArray: UnknownRecord[]) => {
-      if (!isNodeFunction(target)) {
-        throw new Error("Unknow function structure");
-      }
-      const nextTarget = target(...argArray);
-      const nextKeys = [...keys];
-      if (
-        argArray[0] &&
-        !(isMutationNode(nextTarget) || isQueryNodeUndefinedInput(nextTarget))
-      ) {
-        nextKeys.push(argArray[0]);
-      }
-      if (isNode(nextTarget)) {
-        return createProxyNode(nextTarget, nextKeys);
+      if (isNodeFunction(target)) {
+        const nextTarget = target(...argArray);
+        const nextKeys = [...keys];
+        if (
+          argArray[0] &&
+          !(isMutationNode(nextTarget) || isQueryNodeUndefinedInput(nextTarget))
+        ) {
+          nextKeys.push(argArray[0]);
+        }
+        if (isNode(nextTarget)) {
+          return createProxyNode(nextTarget, nextKeys);
+        }
       }
     },
   }) as unknown as ProxyNode<T>;
